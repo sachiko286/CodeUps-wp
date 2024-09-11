b6d52aef00e73f37c9ae3e16d6a51300857e914b
sachiko.t <sattsuttsu66-1@yahoo.co.jp>
2024-09-11 15:39:20 +0900

    20240911-2

--- WordPressTheme/single.php
+++ WordPressTheme/single.php
@@ -8,10 +8,12 @@
 
   <?php get_template_part('parts/breadcrumb') ?>
 
-  <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?> <section class="page-sub-blog top-page-sub-blog">
-        <div class="page-sub-blog__inner inner">
-          <div class="page-sub-blog__main">
+
+  <section class="page-sub-blog top-page-sub-blog">
+    <div class="page-sub-blog__inner inner">
+      <div class="page-sub-blog__main">
+        <?php if (have_posts()) : ?>
+          <?php while (have_posts()) : the_post(); ?>
             <div class="page-sub-blog__heading">
               <time datetime="<?php the_time('c'); ?>" class="page-sub-blog__date"><?php the_time('Y.m/d'); ?></time>
               <h1 class="page-sub-blog__title"><?php the_title(); ?></h1>
@@ -23,44 +25,38 @@
                 <?php endif; ?>
               </figure>
             </div>
-
             <div class="page-sub-blog__body">
               <?php the_content(); ?>
             </div>
-
-            <?php
-            $prev = get_previous_post();
-            if (!empty($prev)) {
-              $prev_url = esc_url(get_permalink($prev->ID));
-            }
-
-            $next = get_next_post();
-            if (!empty($next)) {
-              $next_url = esc_url(get_permalink($next->ID));
-            }
-            ?>
-
-            <div class="pagenavi pagenavi--sub-blog">
-              <div class="pagenavi__inner pagenavi__inner--sub-blog">
-                <div class="wp-pagenavi wp-pagenavi--sub-blog" role="navigation">
-                  <?php if (!empty($prev)) : ?>
-                    <a class="previouspostslink" rel="prev" href="<?php echo $prev_url; ?>"></a>
-                  <?php endif; ?>
-                  <?php if (!empty($next)) : ?>
-                    <a class="nextpostslink" rel="next" href="<?php echo $next_url; ?>"></a>
-                  <?php endif; ?>
-                </div>
-              </div>
+          <?php endwhile; ?>
+        <?php endif; ?>
+        <?php
+        $prev = get_previous_post();
+        if (!empty($prev)) {
+          $prev_url = esc_url(get_permalink($prev->ID));
+        }
+        $next = get_next_post();
+        if (!empty($next)) {
+          $next_url = esc_url(get_permalink($next->ID));
+        }
+        ?>
+        <div class="pagenavi pagenavi--sub-blog">
+          <div class="pagenavi__inner pagenavi__inner--sub-blog">
+            <div class="wp-pagenavi wp-pagenavi--sub-blog" role="navigation">
+              <?php if (!empty($prev)) : ?>
+                <a class="previouspostslink" rel="prev" href="<?php echo $prev_url; ?>"></a>
+              <?php endif; ?>
+              <?php if (!empty($next)) : ?>
+                <a class="nextpostslink" rel="next" href="<?php echo $next_url; ?>"></a>
+              <?php endif; ?>
             </div>
-
           </div>
-
-          <?php get_sidebar(); ?>
-
         </div>
-      </section>
-    <?php endwhile; ?>
-  <?php endif; ?>
+      </div>
+      <?php get_sidebar(); ?>
+    </div>
+  </section>
+
 
 </main>
 
